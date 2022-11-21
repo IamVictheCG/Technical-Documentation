08f24dfaac95a48855e983a1290f0f0c06955ead
Victor Okechukwu <VicOke360@gmail.com>
2022-11-21 18:14:46 +0100

    updates made

--- show.js
+++ show.js
@@ -2,15 +2,16 @@ let html = document.getElementById("html_nav-list")
 let css = document.getElementById("css_nav-list")
 
 
+let js =document.querySelector("#js_Nav")
+let jsNav = document.querySelector(".js_nav-list")
 
-
-js.addEventListener("click", jsnav())
-
-function jsnav() {
-    let js = document.getElementsByClassName("js_nav")
-    // jsNav.classList.toggle("js_nav-list1")
-    if (i = 0, i<js.length, i++) {       
-        js[i].classList.toggle === "js_nav-list1"
+js.addEventListener("click", function jsNav1(e) {
+    if (e.target.tagName === "P") {
+        e.target.classList.toggle('js_nav-list1')
     }
-}
-jsnav()
+    
+    // jsNav.classList.toggle("jsNav_nav-list1")
+    // if (i = 0, i<jsNav.length, i++) {       
+    //     jsNav[i].style.display ='block'
+    // }
+});
